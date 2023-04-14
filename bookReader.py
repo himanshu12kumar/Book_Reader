47e41f8cb1122991ad6208b14d7c041108b31f9b
Himanshu Kumar <ht514440@gmail.com>
2023-04-14 12:27:41 -0700

    Update bookReader.py

--- bookReader.py
+++ bookReader.py
@@ -1,12 +1,13 @@
 import pyttsx3
 import PyPDF2
 book = open('quantum.pdf' , 'rb')
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
-print(pages)
+pdfReader = PyPDF2.PdfReader(book)
+pages1 = len(pdfReader.pages)
+print(pages1)
 speaker = pyttsx3.init()
-page = pdfReader.getPage(1)
+page = pdfReader.pages[1]
 text = page.extract_text()
+print(text)
 voices = speaker.getProperty('voices')
 speaker.setProperty('voice' , voices[1].id)
 rate = speaker.getProperty('rate')
